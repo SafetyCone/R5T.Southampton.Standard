72cf8cf8aeef945ec4cb937468f2324063457666
MinexAutomation <minex.automation@gmail.com>
2019-12-21 14:40:25 -0800

    Added configuration-builder and service-collection extensions.

---
+++ source/R5T.Southampton.Standard/Code/Extensions/IConfigurationBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+
+using R5T.Southampton.Larissa.Standard;
+
+
+namespace R5T.Southampton.Standard
+{
+    public static class IConfigurationBuilderExtensions
+    {
+        public static IConfigurationBuilder AddSourceControlOperatorConfiguration(this IConfigurationBuilder configurationBuilder, IServiceProvider configurationServiceProvider)
+        {
+            configurationBuilder
+                .AddSvnSourceControlOperatorConfiguration(configurationServiceProvider)
+                ;
+
+            return configurationBuilder;
+        }
+    }
+}
---
+++ source/R5T.Southampton.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Southampton.Larissa.Standard;
+
+
+namespace R5T.Southampton.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSourceControlOperator(this IServiceCollection services)
+        {
+            services
+                .AddSvnSourceControlOperator()
+                ;
+
+            return services;
+        }
+    }
+}
